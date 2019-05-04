3b29f76f804381006dc209da2245834ac906940c
Vicente <vic.lule@googlemail.com>
2019-05-04 12:06:17 +0200

    new workflows in circleci

--- src/models/regression_model/tests/test_predict.py
+++ src/models/regression_model/tests/test_predict.py
@@ -5,31 +5,26 @@ from regression_model.processing.data_management import load_dataset
 
 
 def test_make_single_prediction():
-    # Given
+    """Test a prediction is correct"""
+    # load test file
     test_data = load_dataset(file_name='test.csv')
+    # make a single predicition
     single_test_input = test_data[0:1]
-
-    # When
     subject = make_prediction(input_data=single_test_input)
-
-    # Then
     assert subject is not None
     assert isinstance(subject.get('predictions')[0], float)
     assert math.ceil(subject.get('predictions')[0]) == 112476
 
 
 def test_make_multiple_predictions():
-    # Given
+    """Test a prediction is correct"""
+    # load test file
     test_data = load_dataset(file_name='test.csv')
     original_data_length = len(test_data)
+    # make a prediciton for all the testset
     multiple_test_input = test_data
-
-    # When
     subject = make_prediction(input_data=multiple_test_input)
-
-    # Then
     assert subject is not None
     assert len(subject.get('predictions')) == 1451
-
-    # We expect some rows to be filtered out
+    # It is expected some rows to be filtered out
     assert len(subject.get('predictions')) != original_data_length
