f021d16d86687133c383d0d5caac248efcc1abc9
Vicente Guerrero <vicente.guerrero@leybold.com>
2019-05-06 17:21:22 +0200

    error in data_science unittest corrected

--- src/data_science/tests/tests.py
+++ src/data_science/tests/tests.py
@@ -3,7 +3,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import unittest
 
 # from tests.test_data_transfer import DataTransferTest
-from tests.tools.test_transformations import TransformationsTest
+from tools.test_transformations import TransformationsTest
 
 
 if __name__ == '__main__':
