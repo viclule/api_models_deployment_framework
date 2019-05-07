05ffda00f3ec13e2774bd397cfbc27d6cb74bc26
Vicente Guerrero <vicente.guerrero@leybold.com>
2019-05-07 10:27:32 +0200

    unittest for data_science configured

--- src/data_science/tests/tests.py
+++ src/data_science/tests/tests.py
@@ -2,9 +2,7 @@ import os
 import sys
 import unittest
 
-# from tests.test_data_transfer import DataTransferTest
-from tools.test_transformations import TransformationsTest
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from .tools.test_transformations import TransformationsTest
 
 
 if __name__ == '__main__':
--- src/data_science/tests/tools/test_transformations.py
+++ src/data_science/tests/tools/test_transformations.py
@@ -1,5 +1,6 @@
 import unittest
-
+#import sys, os
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from data_science.tools.transformations import per_hour_to_per_second
 #from ...tools.transformations import per_hour_to_per_second
 
